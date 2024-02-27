2b10bd0e88a52ce07cef418f531d1afb38dd9856
AllaVict <allavsct@ukr.net>
2024-02-27 09:23:05 +0200

    #8_advert_contr

--- src/main/java/quick/click/core/service/impl/AdvertEditingServiceImpl.java
+++ src/main/java/quick/click/core/service/impl/AdvertEditingServiceImpl.java
@@ -2,12 +2,8 @@ package quick.click.core.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import quick.click.commons.exceptions.ResourceNotFoundException;
+import quick.click.commons.exeptions.ResourceNotFoundException;
 import quick.click.core.converter.TypeConverter;
 import quick.click.core.domain.dto.AdvertReadDto;
 import quick.click.core.domain.dto.AdvertEditingDto;
