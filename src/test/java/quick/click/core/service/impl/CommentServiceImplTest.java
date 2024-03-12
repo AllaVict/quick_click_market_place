31ec5f782a98e58cc7dcc69e7c935b8196cf4685
AllaVict <allavsct@ukr.net>
2024-03-12 15:19:09 +0200

    #11_comm_serv

---
+++ src/test/java/quick/click/core/service/impl/CommentServiceImplTest.java
@@ -0,0 +1,28 @@
+package quick.click.core.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentServiceImplTest {
+
+    @Test
+    void registerComment() {
+    }
+
+    @Test
+    void getAllCommentsForAdvert() {
+    }
+
+    @Test
+    void editComment() {
+    }
+
+    @Test
+    void deleteComment() {
+    }
+
+    @Test
+    void updateCommentData() {
+    }
+}
